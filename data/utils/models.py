01eac7d271e557b58d601ca10092ace4fc717a3b
Andrés Soto <andres.sotoh@pragma.com.co>
2023-05-16 21:09:46 -0500

    update the model definition

--- data/utils/models.py
+++ data/utils/models.py
@@ -7,37 +7,30 @@ Base = declarative_base()
 #dimensiones
 
 class Pais(Base):
-    """Class for model declaration ENPS Questions
-    """
+
     __tablename__ = 'dimm_pais'
-   # __table_args__ = {'schema': 'pragma_gsheets'}
+
     id = Column(Integer, primary_key=True)
     nombre_pais = Column(String, nullable=False)
-
-
 
     def __init__(self, nombre_pais) -> None:
         self.nombre_pais = nombre_pais
 
 
 class Sexo(Base):
-    """Class for model declaration ENPS Questions
-    """
+
     __tablename__ = 'dimm_sexo'
-   # __table_args__ = {'schema': 'pragma_gsheets'}
+
     id = Column(Integer, primary_key=True)
     desc_sexo = Column(String, nullable=False)
-
-
 
     def __init__(self,desc_sexo) -> None:
         self.desc_sexo = desc_sexo
 
 class Situacion_laboral(Base):
-    """Class for model declaration ENPS Questions
-    """
+
     __tablename__ = 'dimm_situacion_laboral'
-   # __table_args__ = {'schema': 'pragma_gsheets'}
+
     id = Column(Integer, primary_key=True)
     desc_situacion_laboral = Column(String, nullable=False)
 
@@ -46,10 +39,10 @@ class Situacion_laboral(Base):
 
 
 class Rango_edad(Base):
-    """Class for model declaration ENPS Questions
-    """
+
+
     __tablename__ = 'dimm_rango_edad'
-   # __table_args__ = {'schema': 'pragma_gsheets'}
+
     id = Column(Integer, primary_key=True)
     desc_rango_edad = Column(String, nullable=False)
 
@@ -60,7 +53,7 @@ class Rango_edad(Base):
 class Tipo_universidad(Base):
 
     __tablename__ = 'dimm_tipo_universidad'
-   # __table_args__ = {'schema': 'pragma_gsheets'}
+
     id = Column(Integer, primary_key=True)
     desc_tipo_universidad = Column(String, nullable=False)
 
@@ -71,7 +64,7 @@ class Tipo_universidad(Base):
 class Universidades(Base):
 
     __tablename__ = 'dimm_universidades'
-   # __table_args__ = {'schema': 'pragma_gsheets'}
+
     id = Column(Integer, primary_key=True)
     nombre_universidad = Column(String, nullable=False)
     tipo_universidad = Column(String, nullable=False)
@@ -89,7 +82,7 @@ class Universidades(Base):
 class Rama_enseñanza(Base):
 
     __tablename__ = 'dimm_rama_enseanza'
-   # __table_args__ = {'schema': 'pragma_gsheets'}
+
     id = Column(Integer, primary_key=True)
     nombre_rama = Column(String, nullable=False)
 
@@ -101,7 +94,7 @@ class Rama_enseñanza(Base):
 class Ambito_enseñanza(Base):
 
     __tablename__ = 'dimm_ambito_enseanza'
-   # __table_args__ = {'schema': 'pragma_gsheets'}
+
     id = Column(String, primary_key=True)
     desc_ambito = Column(String, nullable=False)
     id_rama = Column(String, nullable=False)
@@ -120,11 +113,10 @@ class Ambito_enseñanza(Base):
 #facts
 
 class Fact_international_graduated(Base):
-    """Class for model declaration ENPS Questions
-    """
+
     __tablename__ = 'fact_international_graduated'
-   # __table_args__ = {'schema': 'pragma_gsheets'}
-    year = Column(Integer, nullable=False)
+
+    year = Column(Integer, primary_key=True)
     id_country = Column(Integer, nullable=False)
     num_graduated_male = Column(Integer, primary_key=True)
     num_graduated_female = Column(Integer, primary_key=True)
@@ -151,38 +143,68 @@ class Fact_international_graduated(Base):
         self.percentage_youth_graduated = percentage_youth_graduated
 
 
+class Fact_egresados_niveles(Base):
 
-class Fact_egresados_rama_enseñanza(Base):
-    """Class for model declaration ENPS Questions
-    """
-    __tablename__ = 'fact_egresados_rama_enseanza'
-   # __table_args__ = {'schema': 'pragma_gsheets'}
-    year = Column(Integer, nullable=False)
-    id_pais = Column(Integer, nullable=False)
-    id_universidad = Column(Integer, nullable=False)
-    id_rama_enseanza = Column(Integer, primary_key=True)
-    num_egresados = Column(Integer, primary_key=True)
-   
+    __tablename__ = 'fact_egresados_niveles'
+
+    year = Column(Integer, primary_key=True)
+    id_pais = Column(Integer, primary_key=True)
+    id_ambito = Column(String, nullable=False)
+    id_sexo = Column(Integer, primary_key=True)
+    id_rango_edad = Column(Integer, nullable=False)
+    num_egresados_nivel_1 = Column(Integer, nullable=False)
+    num_egresados_nivel_2 = Column(Integer, nullable=False)
+    num_egresados = Column(Integer, nullable=False)
 
     def __init__(self,
                  year,
                  id_pais,
-                id_universidad,
-                 id_rama_enseanza,
-                 num_egresados,
-                 ) -> None:
+                 id_ambito,
+                 id_sexo,
+                 id_rango_edad,
+                 num_egresados_nivel_1,
+                 num_egresados_nivel_2,
+                 num_egresados
+    ) -> None:
+
         self.year = year
         self.id_pais = id_pais
-        self.id_rama_enseanza = id_rama_enseanza
+        self.id_ambito = id_ambito
+        self.id_sexo = id_sexo
+        self.id_rango_edad = id_rango_edad
+        self.num_egresados_nivel_1 = num_egresados_nivel_1
+        self.num_egresados_nivel_2 = num_egresados_nivel_2
         self.num_egresados = num_egresados
-        self.id_universidad = id_universidad
 
 
-# DROP TABLE IF EXISTS fact_egresados_rama_enseñanza;
-# CREATE TABLE IF NOT EXISTS fact_egresados_rama_enseñanza (
-# 	año INT NOT NULL,
-# 	id_pais INT,
-#     id_universidad INT,
-#     id_rama_enseñanza INT,
-#     num_egresados INT
-# );
+class Fact_situacion_laboral_egresados(Base):
+
+    __tablename__ = 'fact_situacion_laboral_egresados'
+
+    year = Column(Integer, nullable=False)
+    id_pais = Column(Integer, primary_key=True)
+    id_tipo_universidad = Column(Integer, nullable=False)
+    id_area_estudio = Column(String, nullable=False)
+    id_sexo = Column(Integer, nullable=False)
+    id_situacion_laboral = Column(Integer, nullable=False)
+    cantidad = Column(Integer, nullable=False)
+
+    def __init__(self,
+                 year,
+                 id_pais,
+                 id_tipo_universidad,
+                 id_area_estudio,
+                 id_sexo,
+                 id_situacion_laboral,
+                 cantidad
+    ) -> None:
+
+            self.year = year
+            self.id_pais = id_pais
+            self.id_tipo_universidad = id_tipo_universidad
+            self.id_area_estudio = id_area_estudio
+            self.id_sexo = id_sexo
+            self.id_situacion_laboral = id_situacion_laboral
+            self.cantidad = cantidad
+
+
