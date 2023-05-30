1d350946c99e7bae71350f274f99a99c8de8dd6e
Andres Soto <andres.sotoh@pragma.com.co>
2023-05-30 08:58:38 -0500

    implement custom module load

--- dags/helpers/__init__.py
+++ dags/helpers/__init__.py
@@ -1,4 +1,3 @@
-from .helper import preprocesar_archivo_situacion_laboral, cargar_archivo_situacion_laboral
 from .load_file1 import *
 from .load_file2 import *
 from .load_file3 import *
@@ -6,3 +5,4 @@ from .load_file4 import *
 from .load_file5 import *
 from .load_file6 import *
 from .load_dims import *
+from .load_fact import *
